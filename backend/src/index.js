f6478e45a9d050e177df2a8bdf7f9595da02c16c
Pranjal Sharma <pranjalalone2@gmail.com>
2026-07-01 14:43:20 +0530

    some fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -8,15 +9,13 @@ import { app, server } from "./lib/socket.js";
 import authRoutes from "./routers/auth.route.js";
 import messageRoutes from "./routers/message.route.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT;
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.set("trust proxy", 1);
 
-const allowedOrigins = ["http://localhost:5173" , process.env.CLIENT_URL];
+const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_URL];
 app.use(
   cors({
     origin: allowedOrigins,
