7bf8c46f1aede08083681f629cce14d8d61dcee3
Pranjal Sharma <pranjalalone2@gmail.com>
2026-07-01 15:19:08 +0530

    cookie error fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,6 +9,7 @@ export const generateJWT = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV !== "development" ? "none" : "lax",
   });
 
   return token;
